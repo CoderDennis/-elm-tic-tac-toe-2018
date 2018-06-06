1fda38370cd2f6dba59862df3736b11f4614cf87
CoderDennis <dodava@gmail.com>
2018-06-06 11:28:15 -0500

    adds a test and displays current player

--- src/Main.elm
+++ src/Main.elm
@@ -3,19 +3,57 @@ module Main exposing (..)
 import Html exposing (Html)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Svg.Events exposing (onClick)
+import List exposing (..)
+import List.Extra exposing (..)
+
+
+type Square
+    = X
+    | O
+    | Empty
+
+
+type alias Grid =
+    List Square
+
+
+type Player
+    = PlayerX
+    | PlayerO
+    | Cat
 
 
 type alias Model =
-    String
+    { grid : Grid
+    , player : Player
+    }
 
 
 type Msg
-    = NoOp
+    = Select Int Int
+
+
+type GameState
+    = InProgress
+    | Won Player
+
+
+emptyGrid : Grid
+emptyGrid =
+    repeat 9 Empty
 
 
 init : Model
 init =
-    "Hello Worksoft!"
+    { grid = emptyGrid
+    , player = PlayerX
+    }
+
+
+gameState : Grid -> GameState
+gameState grid =
+    InProgress
 
 
 view : Model -> Html Msg
@@ -26,16 +64,68 @@ view model =
         , height "100%"
         , width "100%"
         ]
-        viewLines
+        [ text_
+            [ x "20"
+            , y "30"
+            , fontFamily "Verdana"
+            , fontSize "35"
+            ]
+            [ text <| toString model.player ]
+        , g [ transform "translate(0,50)" ]
+            [ viewLines
+            , viewGrid model.grid
+            ]
+        ]
 
 
-viewLines : List (Svg Msg)
+viewGrid : Grid -> Svg Msg
+viewGrid grid =
+    g []
+        (grid
+            |> groupsOf 3
+            |> List.indexedMap viewRow
+            |> List.concat
+        )
+
+
+viewRow : Int -> List Square -> List (Svg Msg)
+viewRow rowIndex row =
+    row
+        |> List.indexedMap (viewSquare rowIndex)
+
+
+viewSquare : Int -> Int -> Square -> Svg Msg
+viewSquare row col square =
+    case square of
+        Empty ->
+            rect
+                [ x (toString <| row * 100)
+                , y (toString <| col * 100)
+                , width "100"
+                , height "100"
+                , fillOpacity "0"
+                , onClick (Select row col)
+                ]
+                []
+
+        sq ->
+            text_
+                [ x (toString <| row * 100 + 40)
+                , y (toString <| col * 100 + 70)
+                , fontFamily "Verdana"
+                , fontSize "50"
+                ]
+                [ text <| toString sq ]
+
+
+viewLines : Svg Msg
 viewLines =
-    [ viewLine 0 100 300 100
-    , viewLine 0 200 300 200
-    , viewLine 100 0 100 300
-    , viewLine 200 0 200 300
-    ]
+    g []
+        [ viewLine 0 100 300 100
+        , viewLine 0 200 300 200
+        , viewLine 100 0 100 300
+        , viewLine 200 0 200 300
+        ]
 
 
 viewLine : Int -> Int -> Int -> Int -> Svg Msg
@@ -54,8 +144,20 @@ viewLine x1_ y1_ x2_ y2_ =
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        NoOp ->
-            model
+        Select row col ->
+            let
+                ( nextPlayer, square ) =
+                    case model.player of
+                        PlayerX ->
+                            ( PlayerO, X )
+
+                        _ ->
+                            ( PlayerX, O )
+
+                newGrid =
+                    setAt (row * 3 + col) square model.grid
+            in
+                { model | grid = newGrid, player = nextPlayer }
 
 
 main =
--- tests/Example.elm
+++ tests/Example.elm
@@ -3,8 +3,44 @@ module Example exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
+import Main exposing (..)
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
+    describe "Tic-Tac-Toe Tests"
+        [ describe "GameState"
+            [ test "Game in progress" <|
+                \() ->
+                    gameState emptyGrid
+                        |> Expect.equal InProgress
+            ]
+        , test "PlayerX won on 2nd row" <|
+            \() ->
+                gameState
+                    [ Empty
+                    , O
+                    , O
+                    , X
+                    , X
+                    , X
+                    , Empty
+                    , Empty
+                    , Empty
+                    ]
+                    |> Expect.equal (Won PlayerX)
+        , test "PlayerO won on 2nd column" <|
+            \() ->
+                gameState
+                    [ Empty
+                    , O
+                    , X
+                    , X
+                    , O
+                    , Empty
+                    , X
+                    , O
+                    , Empty
+                    ]
+                    |> Expect.equal (Won PlayerO)
+        ]
